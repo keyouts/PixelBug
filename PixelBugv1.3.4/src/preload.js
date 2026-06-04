87694bd4d70df6f1cb40d4960cf945da7dcde204
keyouts <admin@keyouts.com>
2026-06-04 15:59:06 -0400

    Add files via upload

---
+++ PixelBugv1.3.4/src/preload.js
@@ -0,0 +1,17 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+const safeInvoke = (channel, payload) => ipcRenderer.invoke(channel, payload);
+
+const api = Object.freeze({
+  saveFile: options => safeInvoke("save-file", options && typeof options === "object" ? { ...options } : {}),
+  openProject: () => safeInvoke("open-project"),
+  getSystemTheme: () => safeInvoke("get-system-theme"),
+  onSystemThemeChanged: callback => {
+    if (typeof callback !== "function") return () => {};
+    const listener = (_event, theme) => callback(theme);
+    ipcRenderer.on("system-theme-changed", listener);
+    return () => ipcRenderer.removeListener("system-theme-changed", listener);
+  }
+});
+
+contextBridge.exposeInMainWorld("pixelBug", api);
