e048be3d29778480eb307d98a0f774b22a1c55a6
keyouts <admin@keyouts.com>
2026-07-13 22:32:03 -0400

    Add files via upload

---
+++ PixelBugv1.3.20/src/preload.js
@@ -0,0 +1,24 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+const safeInvoke = (channel, payload) => ipcRenderer.invoke(channel, payload);
+
+const api = Object.freeze({
+  saveFile: options => safeInvoke("save-file", options && typeof options === "object" ? { ...options } : {}),
+  openProject: () => safeInvoke("open-project"),
+  openVoxelModel: () => safeInvoke("open-voxel-model"),
+  getSystemTheme: () => safeInvoke("get-system-theme"),
+  onSystemThemeChanged: callback => {
+    if (typeof callback !== "function") return () => {};
+    const listener = (_event, theme) => callback(theme);
+    ipcRenderer.on("system-theme-changed", listener);
+    return () => ipcRenderer.removeListener("system-theme-changed", listener);
+  },
+  onBrowserZoomBlocked: callback => {
+    if (typeof callback !== "function") return () => {};
+    const listener = () => callback();
+    ipcRenderer.on("browser-zoom-blocked", listener);
+    return () => ipcRenderer.removeListener("browser-zoom-blocked", listener);
+  }
+});
+
+contextBridge.exposeInMainWorld("pixelBug", api);
